df520e84737c6ab36265c512233431d770a2dad0
Franco Victorio <victorio.franco@gmail.com>
2021-04-14 16:18:51 -0300

    Inline sharedBeforeEach

--- test/test.js
+++ test/test.js
@@ -1,103 +1,84 @@
 const { expect } = require("chai");
 
-describe("describe", () => {
+describe("describe", function () {
+  this.timeout(0);
   let test;
-  sharedBeforeEach("run 13 txs", async () => {
-    const [s] = await ethers.getSigners();
 
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-    await s.sendTransaction({ to: s.address });
-  });
+  {
+    let initialized = false;
+    let snapshot;
+
+    beforeEach("run 13 txs", async () => {
+      if (!initialized) {
+        const [s] = await ethers.getSigners();
+
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+        await s.sendTransaction({ to: s.address });
+
+        initialized = true;
+        snapshot = await takeSnapshot();
+      } else {
+        await revert(snapshot);
+      }
+    });
+  }
 
   describe("inner describe 1", () => {
-    sharedBeforeEach("create test", async () => {
-      test = await deploy("Test");
-    });
     it("reverts", async () => {
+      const test = await deploy("Test");
       await expect(test.fail()).to.be.revertedWith("");
     });
   });
 
   describe("inner describe 2", () => {
-    sharedBeforeEach("create test", async () => {
-      test = await deploy("Test");
-      await test.get();
-    });
+    {
+      let initialized = false;
+      let snapshot;
+      beforeEach("create test", async () => {
+        if (!initialized) {
+          test = await deploy("Test");
+          await test.get();
+
+          initialized = true;
+          snapshot = await takeSnapshot();
+        } else {
+          await revertSnapshot(snapshot);
+        }
+      });
+    }
     it("reverts", async () => {
       await expect(test.fail()).to.be.revertedWith("");
     });
   });
 });
 
-const SNAPSHOTS = [];
-
-function sharedBeforeEach(nameOrFn, maybeFn) {
-  const name = typeof nameOrFn === "string" ? nameOrFn : undefined;
-  const fn = typeof nameOrFn === "function" ? nameOrFn : maybeFn;
-
-  let initialized = false;
-
-  beforeEach(wrapWithTitle(name, "Running shared before each or reverting"), async function () {
-    const provider = await getProvider();
-    if (!initialized) {
-      const prevSnapshot = SNAPSHOTS.pop();
-      if (prevSnapshot !== undefined) {
-        await revert(provider, prevSnapshot);
-        SNAPSHOTS.push(await takeSnapshot(provider));
-      }
-
-      await fn.call(this);
-
-      SNAPSHOTS.push(await takeSnapshot(provider));
-      initialized = true;
-    } else {
-      const snapshotId = SNAPSHOTS.pop();
-      if (snapshotId === undefined) throw Error("Missing snapshot ID");
-      await revert(provider, snapshotId);
-      SNAPSHOTS.push(await takeSnapshot(provider));
-    }
-  });
-
-  after(async function () {
-    if (initialized) {
-      SNAPSHOTS.pop();
-    }
-  });
-}
-
-function wrapWithTitle(title, str) {
-  return title === undefined ? str : `${title} at step "${str}"`;
-}
-
-async function deploy(contract, { from, args } = {}) {
-  if (!args) args = [];
-  if (!from) from = (await ethers.getSigners())[0];
-  const factory = (await getFactory(contract)).connect(from);
-  const instance = await factory.deploy(...args);
+async function deploy(contract) {
+  const factory = await ethers.getContractFactory(contract);
+  const instance = await factory.deploy();
   return instance.deployed();
 }
 
-async function takeSnapshot(provider) {
-  const result = await provider.request({
+async function takeSnapshot() {
+  const result = await network.provider.request({
     method: "evm_snapshot"
   });
 
   return result;
 }
 
-async function revert(provider, snapshotId) {
-  const result = await provider.request({
+async function revert(snapshotId) {
+  const result = await network.provider.request({
     method: "evm_revert",
     params: [snapshotId]
   });
@@ -106,22 +87,4 @@ async function revert(provider, snapshotId) {
     console.trace();
     process.exit(1);
   }
-}
-
-async function getProvider() {
-  const hre = require("hardhat");
-  return hre.network.provider;
-}
-
-const factories = {};
-
-async function getFactory(contractName) {
-  let factory = factories[contractName];
-
-  if (factory == undefined) {
-    factory = await ethers.getContractFactory(contractName);
-    factories[contractName] = factory;
-  }
-
-  return factory;
 }
